1303a1d64e21ea50e666f22288f2ba450a34bd4a
Mathias <mathias_nunez@usmp.pe>
2025-05-23 21:54:36 -0500

    probando y agregando historial de like y dislike

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PC3_Progra1.Data;
 using PC3_Progra1.Models;
+using System.Linq;
 
 namespace PC3_Progra1.Controllers
 {
@@ -22,7 +23,6 @@ namespace PC3_Progra1.Controllers
             {
                 TempData["PostReaccionado"] = postId;
                 TempData["Mensaje"] = "Ya registraste una reacción para esta publicación.";
-
                 return RedirectToAction("Index", "Home");
             }
 
@@ -35,8 +35,19 @@ namespace PC3_Progra1.Controllers
             _context.Feedbacks.Add(feedback);
             _context.SaveChanges();
 
+            TempData["PostReaccionado"] = postId;
             TempData["Mensaje"] = "Gracias por tu reacción.";
             return RedirectToAction("Index", "Home");
+        }
+
+        // Acción para ver el historial de feedbacks
+        public IActionResult Historial()
+        {
+            var historial = _context.Feedbacks
+                .OrderBy(f => f.Id)
+                .ToList();
+
+            return View(historial);
         }
     }
 }
--- obj/Debug/net9.0/PC3-Progra.AssemblyInfo.cs
+++ obj/Debug/net9.0/PC3-Progra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PC3-Progra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ac2d251c0fb89dec2bc0ad2ac52eaa4438e2e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e40878b4da278f5466dbb8cc75c58751a9597e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PC3-Progra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PC3-Progra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
