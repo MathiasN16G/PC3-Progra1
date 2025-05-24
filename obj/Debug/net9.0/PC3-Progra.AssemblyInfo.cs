25ba1c65cdca7a8a4a4a21212d786b5517a54df9
Mathias <mathias_nunez@usmp.pe>
2025-05-23 22:37:01 -0500

    probando finales

--- obj/Debug/net9.0/PC3-Progra.AssemblyInfo.cs
+++ obj/Debug/net9.0/PC3-Progra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PC3-Progra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bda2c6294460588c75d8b06f4a357b8dbf159f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa56f485e2d71a5d49c4f7618141e059102ebca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PC3-Progra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PC3-Progra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
